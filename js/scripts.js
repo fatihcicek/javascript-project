1c43c43eb6849d84ea15d8cfc032801546ca1646
Fatih Cicek <fatihcicekofficial@gmail.com>
2021-06-08 03:25:52 +0300

    Update scripts.js
    Created variable pokemonList with three objects and three properties each

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert('Hello world');
-let favoriteFood = 'Da Panji';
-document.write(favoriteFood);
+let pokemonList=[{name: "Bulbasaur", height: 7, type: "grass"},{name: "Ivysaur", height: 10, type: "monster"},{name: "Pikachu", height: 4, type: "fairy"}]
