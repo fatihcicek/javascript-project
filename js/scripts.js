5c74ce455f507412cdf28a25a70c6e6db0bd319a
Fatih Cicek <fatihcicekofficial@gmail.com>
2021-06-10 01:07:14 +0300

    Achievement 3
    Created a loop, within loop: added conditional, highlighted pokemon with height > 8

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-let pokemonList=[{name: "Bulbasaur", height: 7, type: "grass"},{name: "Ivysaur", height: 10, type: "monster"},{name: "Pikachu", height: 4, type: "fairy"}]
+let pokemonList=[
+  {name: "Bulbasaur", height: 7, type: "grass"},
+  {name: "Ivysaur", height: 10, type: "monster"},
+  {name: "Pikachu", height: 4, type: "fairy"}
+]
+
+for (let i=0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name);
+  if (pokemonList[i].height > 8) {
+    console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!" );
+  }else {
+    console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
+  }
+}
