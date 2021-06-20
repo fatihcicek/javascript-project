bcd56cb5b639db52415427e2f48f7b3762366e5b
Fatih Cicek <fatihcicekofficial@gmail.com>
2021-06-20 19:11:24 +0300

    Update scripts.js
    Task 5
    refactoring: for to forEach
    wrapping pokemonList in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
-let pokemonList=[
-  {name: "Bulbasaur", height: 7, type: "grass"},
-  {name: "Ivysaur", height: 10, type: "monster"},
-  {name: "Pikachu", height: 4, type: "fairy"}
-]
+//wrapping pokemonList array in an IIFE
+let pokemonRepository = (function() {
+  //creating the Pokemon database
+    {name: "Bulbasaur", height: 7, type: "grass"},
+    {name: "Ivysaur", height: 10, type: "monster"},
+    {name: "Pikachu", height: 4, type: "fairy"}
+  ];
 
-for (let i=0; i < pokemonList.length; i++) {
-  //Writes Pokemon name to the DOM
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-  //Checks if pokemon height is greater than 8
-  if (pokemonList[i].height > 8) {
-    //if pokemon height is greater than 1.6 m, writes "Wow that's big!"
-    document.write(" - Wow, that's big!" );
-    }
-    //Adds line breaks after each pokemon name
-    document.write("<br><br>");
-}
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+  pokemonRepository.add({
+    name: 'Balbasaur',
+    height: 5.7,
+    type: ['grass', 'poison']
+  });
+  console.log(pokemonRepository.getAll());
+
+
+pokemonRepository.getAll().forEach(function(item)) {
+  if (item.height > 5) {
+    document.write(item.name + " is " + item.height + " m high" + " - Wow, that's big!" + "<br>");
+      } else {
+        document.write(item.name + " is " + item.height + " m high!" + "<br>");
+      }
+  }
+  })
